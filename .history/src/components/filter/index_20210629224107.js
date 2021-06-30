fd1cb7b43b0e1c853430637bd24cbb691b1eeffd
Jean Janrley <jeanjanrley@outlook.com>
2021-06-29 22:47:09 -0300

    Leitura e listagem dos rotulos funcionando plenamente

---
+++ .history/src/components/filter/index_20210629224107.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import './styles.scss'
+
+export default function(props){
+
+    return(
+        <div className="filter">
+            {props.title == null ? <></> :  <span>{props.title}</span>}
+            {props.subTitle == null ? <></> :  <span className="sub-title">{props.subTitle}</span>}
+
+            <input
+                value={props.value}
+                onChange={(event) => props.setState(event.target.value)}
+                list={props.id}
+                name={props.id}
+                id={props.id}
+                placeholder={props.placeholder}
+                type={props.type == null ? "text" :  props.type}
+                readonly
+
+            />
+            <datalist id={props.id}>
+                <option value={props.value}/>
+             </datalist>
+        </div>
+    )
+} 
---
+++ .history/src/components/rotulo/index_20210629215407.js
@@ -0,0 +1,44 @@
+import React from 'react'
+
+import './style.scss'
+import codigo from '../../assets/icons/codigo.png'
+import save from '../../assets/icons/save.svg'
+
+export default function Rotulo(props){
+    return(
+        <div className="rotulo">
+
+            <div className="title-rotulo">
+                <img src={save} alt="salvar"/>
+                <h2>{props.nome}</h2>
+                <input type="checkbox"/>
+            </div>
+
+            <div className="sub-rotulo">
+                <textarea
+                    value={props.value}
+                    onChange={props.contraRotulo}
+                />
+
+                <div className="rotuloDados">
+                    <div>
+                        <span>Lote:</span> <span>{props.lote}</span>
+                    </div>
+
+                    <div>
+                        <span>D. Prod:</span> <span>{props.data}</span>
+                    </div>
+
+                    <div>
+                        <span>D. Val:</span> <span>{props.validade}</span>
+                    </div>
+                </div>
+
+                <div className="codigo-de-barras">
+                    <img src={codigo} alt="codigo de barras"/>
+                </div>
+                
+            </div>
+        </div>
+    )
+}
